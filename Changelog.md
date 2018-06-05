c26c79046ca8f32c36bfbce9e64c8bf7da8be113
Sebastián Gurin <sebastigurin@gmail.com>
2018-06-04 23:37:45 -0300

    fix run final report

---
+++ Changelog.md
@@ -0,0 +1,5 @@
+## 0.0.11
+
+ * run final report fix
+
+## 0.0.12
--- src/run.ts
+++ src/run.ts
@@ -4,8 +4,11 @@ import { YamatConfig } from ".";
 import { getConfig } from "./util";
 
 /**
- * Runs given command on each package serially. If some fails, abort and exit with that status code
- * TODO: config for running them all no matter if some fails. 
+ * Runs given command on each package serially. 
+ *
+ * By default it will run them all no matter if there are errors (some command ends with exit code
+ * different than zero) and list each commend errors in a final report. If --breakOnError is passed
+ * in which case it will break on first commend error and exit with the same command exit code. 
  */
 export function run(runConfig: RunConfig) {
   console.log(`Running in all packages command : ${JSON.stringify(runConfig)}`);
@@ -17,7 +20,7 @@ export function run(runConfig: RunConfig) {
     const code = exec(runConfig.cmd).code
     if (code !== 0) {
       console.error(`ERROR while trying to execute command "${runConfig.cmd}" in ${c.path}`)
-      if (runConfig.breakOnError) {
+      if (runConfig.breakOnError!==undefined) {
         process.exit(code)
       }
     } else {
@@ -26,8 +29,10 @@ export function run(runConfig: RunConfig) {
     results.push({ cmd: runConfig.cmd, path: c.path, code })
     cd(originalDir)
   })
-  if (results.length) {
-    console.error(`\nERRORs when executing the following commands on some packages: \n${JSON.stringify(results.map(r=>r.code!==0), null, 2)}`)
+  if (results.length && results.find(r => r.code !== 0)) {
+    console.error(`\nERRORs thrown when executing the following commands on some packages: 
+${JSON.stringify(results.filter(r => r.code !== 0), null, 2)}
+    `)
   } else {
     console.log(`Command "${runConfig.cmd}" successfully run in all packages without errors`);
   }
